638f13888db7ded68d6fcb7cb74afcb0c36db2fb
Max <maxim.macari.dam@gmail.com>
2021-01-02 20:25:57 +0100

    Initial Commit

---
+++ VerticalCarousel/VerticalCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCarouselApp.swift
+//  VerticalCarousel
+//
+//  Created by Maxim Macari on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct VerticalCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
